2d1c02f9908afc78b066c5bf687888e4cb4dd535
Anju Ghimire <anjughimire8848@gmail.com>
2024-07-02 14:06:53 +0545

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './portfolio-website/home/home.component';
+import { AboutComponent } from './portfolio-website/about/about.component';
+import { SkillComponent } from './portfolio-website/skill/skill.component';
+import { ContactComponent } from './portfolio-website/contact/contact.component';
+import { FooterComponent } from './portfolio-website/footer/footer.component';
+import { HeaderComponent } from './portfolio-website/header/header.component';
+import { ProjectComponent } from './portfolio-website/project/project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        component : HomeComponent
+    },
+    {
+        path : 'home',
+        redirectTo : 'home',
+        pathMatch : 'full'
+    },
+    {
+        path : 'home',
+        component : HomeComponent
+    },
+    {
+        path : 'header',
+        component : HeaderComponent
+    },
+    {
+        path : 'about',
+        component : AboutComponent
+    },
+    {
+        path : 'skill',
+        component : SkillComponent
+    },
+    {
+        path : 'project',
+        component : ProjectComponent
+    },
+    {
+        path : 'contact',
+        component : ContactComponent
+    },
+    {
+        path : 'footer',
+        component : FooterComponent
+    }
+];
